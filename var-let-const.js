14e5c37c5e78597aa9f0e36d22d5d12e47240c94
qiufeihong2018 <15058301288@163.com>
2019-03-03 16:52:30 +0800

    var-let-const

---
+++ var-let-const.js
@@ -0,0 +1,28 @@
+// 提升
+// console.log(a)//undefined
+// var a = 10
+
+// 从上述代码中我们可以发现，虽然变量还没有被声明，但是我们却可以使用这个未被声明的变量，这种情况就叫做提升，并且提升的是声明
+// 上面等于下面
+// var a
+// console.log(a)//undefined
+// a=10
+
+
+// var a
+// var a
+// a=10
+// console.log(a)
+// 上面等于下面
+
+// var a=11
+// var a
+// console.log(a)
+//
+// console.log(a)//[Function: a]
+// function a() {
+//
+// }
+// var a=11
+// 函数会被提升且优于变量提升
+
